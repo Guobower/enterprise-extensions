d8b37b240eace08d6ba1b9b481cff8adc12fabdf
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-05-02 16:20:07 -0300

    [IMP] print button on invoices on proforma state

--- account_usability/__openerp__.py
+++ account_usability/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Accoount Usability Improvements',
-    'version': '9.0.1.15.0',
+    'version': '9.0.1.16.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',

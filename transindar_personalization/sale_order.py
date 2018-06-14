8e1530b568697ee0963c80525375ad2a5241a3c7
Juan José Scarafía <jjs@adhoc.com.ar>
2018-06-14 19:32:33 -0300

    [ADD] on boggio, returns use second currency and confirmation date exchange rate (#63)

--- transindar_personalization/sale_order.py
+++ transindar_personalization/sale_order.py
@@ -19,6 +19,62 @@ class SaleOrder(models.Model):
         string='Tiempo De Preparacion')
 
     @api.multi
+    def action_invoice_create(self, grouped=False, final=False):
+        """Quieren que cuando se haga una devolucion, si se factura en dolares,
+        se use la cotizacion de la confirmación de la venta (para no acreditar)
+        mayor importe en pesos del que pago.
+        Es un metodo medio engorroso ya que se pueden crear facturas desde
+        varias ventas pero luego se devuelve un listado de facturas en total y
+        nosotros queremos saber para las que son completamente devoluciones,
+        la cotización a la fecha de la orden y del listado de factura no
+        sabriamos cual es la orden de venta, por eso empezamos recorriendo
+        las ordenes de venta.
+        NOTA: si se quiere generalizar esto tener en cuenta que tal vez desde
+        una OV en una moneda se podría estar facturando en esa moneda en otra
+        cia que tenga esa moneda como base y habria algun tipo de inconsistencia
+        """
+        invoice_ids = []
+
+        if final:
+            other_currency_sales = self.filtered(
+                lambda x: x.currency_id != x.company_id.currency_id)
+            self -= other_currency_sales
+
+            for other_currency_sale in other_currency_sales:
+                invoice_ids += super(
+                    SaleOrder, other_currency_sale).action_invoice_create(
+                    grouped=grouped, final=final)
+                for refund in self.env['account.invoice'].browse(
+                        invoice_ids).filtered(lambda x:
+                            x.type == 'out_refund' and
+                            x.currency_id != x.company_id.currency_id):
+
+                    refund_currency = refund.currency_id
+                    company_currency = refund.company_id.currency_id
+                    rate = refund_currency.with_context(
+                        date=other_currency_sale.date_confirm).compute(
+                            1.0, company_currency)
+
+                    self.env['account.change.currency'].with_context(
+                        active_id=refund.id).create({
+                            'currency_id': company_currency.id,
+                            'currency_rate': rate,
+                        }).change_currency()
+
+                    # TODO en v11 esto ya lo podria resolver el wizard
+                    # directamente
+                    refund.update({
+                        'move_currency_id': refund_currency.id,
+                        'move_inverse_currency_rate': rate,
+                    })
+
+        # self podria no tener elementos a estas alturas
+        if self:
+            invoice_ids += super(SaleOrder, self).action_invoice_create(
+                grouped=grouped, final=final)
+        return invoice_ids
+
+    @api.multi
     def action_confirm(self):
         param = self.env['ir.config_parameter'].get_param(
                 'sale_order_action_confirm')
